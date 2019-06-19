2d150fffa46eeb5dc1543d13019e650ab837d070
Nikita Skubak <skubak@moqod.com>
2019-06-19 16:03:52 +0300

    setup base project functionality

--- sample/src/main/java/ru/breffi/storyiot/MainActivity.kt
+++ sample/src/main/java/ru/breffi/storyiot/MainActivity.kt
@@ -1,8 +1,8 @@
 package ru.breffi.storyiot
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
