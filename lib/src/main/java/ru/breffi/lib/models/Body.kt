0acb1e120769bb93755b8a6a0b72eaa0e62c6622
Nikita Skubak <skubak@moqod.com>
2019-06-19 15:51:59 +0300

    setup base project functionality

--- lib/src/main/java/ru/breffi/lib/models/Body.kt
+++ lib/src/main/java/ru/breffi/lib/models/Body.kt
@@ -1,14 +1,9 @@
 package ru.breffi.lib.models
 
-import com.google.gson.annotations.Expose
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
 open class Body : RealmObject() {
-    @Expose
     @PrimaryKey
     var id : String? = null
-
-    @Expose
-    var value : String? = null
 }
